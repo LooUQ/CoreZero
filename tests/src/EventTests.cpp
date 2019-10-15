1af5b4a6553bdded7aca22a1701b656028c967e8
Jensen Miller <jensen@loouq.net>
2019-10-15 01:32:07 -0400

    Uni-directional and Bi-directional signaling tested

--- tests/src/EventTests.cpp
+++ tests/src/EventTests.cpp
@@ -28,7 +28,28 @@ namespace CoreZero
 {
 	using TestHandlerType = Delegate<void(int)>;
 
+	/** Events are typically get constructed in classes
+	 *	however, they aren't always subscribed to. Thus
+	 *	we need to test an unsubscribed event for 
+	 *	being fired with the possiblity of no subscriber
+	 *	and NOT throw an exception*/
+	TEST(EventTest, ConstructUnsubscribed)
+	{
+		Event<TestHandlerType> test_event;
+		test_event(7);
+		//	Nothing should happen
+		//		Events should be constructable
+		//		without being aggregated and
+		//		not throw an error.
+		SUCCEED();
+	}
+
+
+	/// A free signal variable for checking.
 	int free_signal = 0;
+
+	/** The method to execute on a given signal.
+	 *	\param[in] val An argument to pass to the handler. */
 	void OnSignal(int val)
 	{
 		//Logger::WriteMessage("Trace: $freeHandler\n");
@@ -38,15 +59,43 @@ namespace CoreZero
 		free_signal = 1;
 	}
 
-	TEST(EventTest, ConstructAndFire)
+
+
+	/**	Typical usage of an event includes a subscriber method
+	 *	that gets called upon a signal. This is a uni-directional
+	 *	signal, in which one object signals another object or method. */
+	TEST(EventTest, SubscribeAndFire)
 	{
 		//	construct event
-		Event<TestHandlerType> test_event;
+		Event<TestHandlerType> something_signaled;
 		//	subscribe to static/free handler
-		test_event += OnSignal;
+		something_signaled += OnSignal;
 		//	fire event (issue call)
-		test_event(7);
+		something_signaled(7);
 		//	assert the handler was fired
 		Assert::IsTrue(free_signal);		
+	}
+
+	using Accumulator = Delegate<int(int, int)>;
+
+	/** Sum two integers.
+	 *	\param[in] x An integer.
+	 *	\param[in] y An integer. */
+	int Sum(int x, int y)
+	{
+		return x + y;
+	}
+
+	/** Alternatively, events can be used for bi-directional signaling, where
+	 *	the signaler can pass data to the subscriber, and the subscriber can
+	 *	return a value to the event without knowing who the event belongs to. */
+	TEST(EventTest, EventWithReturingSubscriber)
+	{
+		//	construct event
+		Event<Accumulator> accumulating_event;
+		//	subscribe
+		accumulating_event += Sum;
+		//	check the RETURN value of the subscriber method
+		Assert::AreEqual(12, accumulating_event(5, 7));
 	}
 }
